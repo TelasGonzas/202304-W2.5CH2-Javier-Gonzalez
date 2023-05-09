b40c67274422f200442e4640c0624371f6aab5d5
Javier González <jgestarriol@gmail.com>
2023-05-10 00:28:42 +0200

    Correct string use on super keywords

--- got-classes/adviser.js
+++ got-classes/adviser.js
@@ -2,7 +2,7 @@ import { Character } from "./character.js";
 
 export class Adviser extends Character {
   constructor(name, family, age, alive, characterToAdvise) {
-    super("name", "family", "age");
+    super(name, family, age);
     this.characterToAdvise = characterToAdvise;
     this.sentence = "No sé por qué, pero creo que voy a morir pronto";
   }
